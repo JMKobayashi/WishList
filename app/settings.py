23aa7105acca0589a3131df000e0bfe941da4889
JKobayashi98 <james.m.kobayashi@gmail.com>
2021-03-21 19:11:04 -0300

    starting database

--- app/settings.py
+++ app/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'wishlist',
 ]
 
 MIDDLEWARE = [
@@ -50,7 +51,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'WishList.urls'
+ROOT_URLCONF = 'app.urls'
 
 TEMPLATES = [
     {
@@ -68,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'WishList.wsgi.application'
+WSGI_APPLICATION = 'app.wsgi.application'
 
 
 # Database
--- wishlist/models.py
+++ wishlist/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class WishListModel(models.Model):
+    title = models.CharField(max_length=20,blank=False)
+    description = models.CharField(max_length=50,blank=True)
+    link = models.CharField(max_length=2000,blank=True)
+    image_like = models.CharField(max_length=2000,blank=True)
